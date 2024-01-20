33871ade84c6eac4e7c4fd9524b63e95f47707b7
Ayan Pal <140514541+MESS00N@users.noreply.github.com>
2024-01-20 21:51:28 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,14 +1,1 @@
-const http = require('http');
-
-const hostname = '13.201.104.236';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World from nodejs\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+console.log("Hello!  from Node JS");
